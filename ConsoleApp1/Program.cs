7a128136154a5abd4c2255ab5e2e8644ce78ff03
Riley Procopio <rmp8323@rit.edu>
2021-02-12 12:03:26 -0500

    Made LargestOfThree method

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -18,6 +18,38 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            LargestOfThree();
+
+            Console.ReadLine();
+
+            // Asks the user for three numbers and finds the highest of the three
+            void LargestOfThree() 
+            {
+                int highestNum;
+                int currentNum;
+
+                Console.WriteLine("Welcome to the Largest Of Three! Enter three numbers!");
+
+                Console.WriteLine("1st Number: ");
+                currentNum = int.Parse(Console.Readline());
+                highestNum = currentNum;
+
+                Console.WriteLine("2nd Number: ");
+                currentNum = int.Parse(Console.Readline());
+                if (currentNum > highestNum)
+                {
+                    highestNum = currentNum;
+                }
+
+                Console.WriteLine("3rd Number: ");
+                currentNum = int.Parse(Console.Readline());
+                if (currentNum > highestNum)
+                {
+                    highestNum = currentNum;
+                }
+
+                Console.WriteLine("The highest number was " + highestNum + "!");
+            }
         }
     }
 }
